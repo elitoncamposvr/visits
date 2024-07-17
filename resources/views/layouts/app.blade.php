b15596fa9cd2f89646ec86d89899734a01d56f5f
Eliton Campos <eliton.pecas@gmail.com>
2024-07-16 22:41:50 -0300

    Layout Init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,59 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body class="min-h-screen font-sans antialiased bg-base-200/50 dark:bg-base-200">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    {{-- NAVBAR mobile only --}}
+    <x-mary-nav sticky class="lg:hidden">
+        <x-slot:brand>
+            <div class="ml-5 pt-5">App</div>
+        </x-slot:brand>
+        <x-slot:actions>
+            <label for="main-drawer" class="lg:hidden mr-3">
+                <x-mary-icon name="o-bars-3" class="cursor-pointer" />
+            </label>
+        </x-slot:actions>
+    </x-mary-nav>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    {{-- MAIN --}}
+    <x-mary-main full-width>
+        {{-- SIDEBAR --}}
+        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
+
+            {{-- BRAND --}}
+            <div class="ml-5 pt-5">App</div>
+
+            {{-- MENU --}}
+            <x-mary-menu activate-by-route>
+
+                {{-- User --}}
+                @if($user = auth()->user())
+                    <x-mary-menu-separator />
+
+                    <x-mary-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
+                        <x-slot:actions>
+{{--                            <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />--}}
+                        </x-slot:actions>
+                    </x-mary-list-item>
+
+                    <x-mary-menu-separator />
+                @endif
+
+                <x-mary-menu-item title="Hello" icon="o-sparkles"  />
+                <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
+                    <x-mary-menu-item title="Wifi" icon="o-wifi" />
+                    <x-mary-menu-item title="Archives" icon="o-archive-box"  />
+                </x-mary-menu-sub>
+            </x-mary-menu>
+        </x-slot:sidebar>
+
+        {{-- The `$slot` goes here --}}
+        <x-slot:content>
+            {{ $slot }}
+        </x-slot:content>
+    </x-mary-main>
+
+    {{-- Toast --}}
+    <x-mary-toast />
     </body>
 </html>
